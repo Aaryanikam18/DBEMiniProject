63255e04046229a0a3f17085c26b13dc38381625
Aaryanikam18 <aarya292218@gmail.com>
2025-11-18 21:30:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bookstore Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<nav>
+    <a href="index.php">Home</a>
+    <a href="search_books.php">Catalog</a>
+    <a href="place_order.php">Order</a>
+    <a href="add_book.php">Admin (Add Book)</a>
+</nav>
+
+<div class="container">
+    <h2>Welcome to the Bookstore Management System</h2>
+    <p>Select an option from the menu above to manage inventory or place orders.</p>
+</div>
+
+</body>
+</html>
